b789fe513f8cdfc8f43da9cf4a4dab5901404b61
madhavip1026 <madhavip1026@gmail.com>
2026-07-18 12:12:19 -0500

    added header

--- CRUD/src/main/java/com/Springboot/CRUD/Controller/AuthController.java
+++ CRUD/src/main/java/com/Springboot/CRUD/Controller/AuthController.java
@@ -1,33 +1,33 @@
 package com.Springboot.CRUD.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Springboot.CRUD.DTO.LoginRequest;
 import com.Springboot.CRUD.Service.AuthService;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
-    @PostMapping("/auth/login")
-    public String login(
-        @RequestBody LoginRequest request) {
-            System.out.println("LOGIN API HIT");
 
-    return authService.login(
-        request.getEmail(),
-        request.getPassword()
-    );
-}
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+            @RequestHeader("email") String email,
+            @RequestHeader("password") String password) {
 
-    
-    
+        return ResponseEntity.ok(
+                authService.login(email, password)
+        );
+    }
 }
 
 
